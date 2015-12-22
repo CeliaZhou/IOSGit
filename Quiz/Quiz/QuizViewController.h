fed24615f70c89a5a9455cabd79bc04274458b48
Admin <admin@chunyue.local>
2015-12-22 14:12:43 +0800

    creat an ios project

---
+++ Quiz/Quiz/QuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuizViewController.h
+//  Quiz
+//
+//  Created by Admin on 15/12/21.
+//  Copyright © 2015年 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizViewController : UIViewController
+
+@end
