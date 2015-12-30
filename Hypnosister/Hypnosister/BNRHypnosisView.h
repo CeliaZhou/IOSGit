b52e4e7ed8b0128db609ca8d961184ed726aabc8
Admin <admin@chunyue.local>
2015-12-30 15:36:39 +0800

    commit a Hypnosister project

---
+++ Hypnosister/Hypnosister/BNRHypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisView.h
+//  Hypnosister
+//
+//  Created by Admin on 15/12/23.
+//  Copyright © 2015年 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisView : UIView
+
+@end
